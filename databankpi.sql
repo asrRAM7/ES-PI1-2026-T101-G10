abfc2a25ec46db3fa1772fc680bb2c3897c465f0
asrRAM7 <ramiro.asr@puccampinas.edu.br>
2026-04-29 11:05:27 -0300

    Modify 'partido' column size in 'candidatos' table
    Increased the size of the 'partido' column in the 'candidatos' table from varchar(10) to varchar(50). Removed unnecessary alterations and insertions.

--- databankpi.sql
+++ databankpi.sql
@@ -4,7 +4,7 @@ create table candidatos(
 id_candidato int primary key auto_increment, 
 nome_candidato varchar(50),
 numero_votacao int,
-partido varchar(10)
+partido varchar(50)
 );
 create table eleitores(
 id_eleitor int primary key auto_increment,
@@ -15,21 +15,4 @@ mesario int NOT NULL,
 chave_acesso VARCHAR(10)
 );
 
-alter table candidatos
-modify column partido varchar(10),
-modify column nome varchar(50) NOT NULL;
-
-alter table candidatos 
-modify column id int auto_increment;
-
-DELETE from candidatos 
-WHERE nome = 'carlos' and partido = 'pt';
-
-alter table candidatos
-modify column partido varchar(50);
-
-INSERT into candidatos (nome, partido)
-VALUES("Marcos Alberto", "Partido Falso");
-
-INSERT into eleitores (nome, titulo, CPF, mesario)
-VALUES ("Maria Silva", 154327169871, 87537822814, 1);
+SELECT * FROM eleitores;
