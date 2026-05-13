4bd1f49d9e6ebaea0ace90dda58f59ac4a89d150
Gabriel-Jurgensen <gabriel.lj1@puccampinas.edu.br>
2026-05-13 10:15:14 -0300

    Adicionado código de auditoria

---
+++ auditoria.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def registrar_log(entrada):
+    try:
+        with open("logs.txt", "r", encoding="utf-8"):
+            pass
+    except FileNotFoundError:
+        with open("logs.txt", "w", encoding="utf-8") as arquivo:
+            arquivo.write(f"Horário: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} --- Registro: Arquivo de Logs criado.\n")
+            pass
+    
+    with open("logs.txt", "a", encoding="utf-8") as arquivo:
+        arquivo.write(f"Horário: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} --- Registro: {entrada}\n")
+
+def ler_log():
+    try:
+        with open("logs.txt", "r", encoding="utf-8") as arquivo:
+            conteudo = arquivo.read()
+            print(conteudo)
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
