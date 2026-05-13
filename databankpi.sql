87befc545f1ef5ef9afedcd520936e7ce87cdb9a
Gabriel-Jurgensen <gabriel.lj1@puccampinas.edu.br>
2026-05-12 23:50:42 -0300

    Insert candidate and modify eleitores table schema
    Added a new candidate insertion and updated the eleitores table structure.

--- databankpi.sql
+++ databankpi.sql
@@ -6,13 +6,19 @@ nome_candidato varchar(50),
 numero_votacao int,
 partido varchar(50)
 );
+INSERT INTO eleicao_db.candidatos (nome_candidato,numero_votacao,partido)
+VALUES ('Manuel',14,'Partido dos Aposentados');
+
 create table eleitores(
 id_eleitor int primary key auto_increment,
 nome_eleitor varchar(50) NOT NULL,
 titulo_eleitor varchar(12) NOT NULL,
 cpf varchar(11) NOT NULL,
-mesario int NOT NULL,
-chave_acesso VARCHAR(10)
+mesario INT NOT NULL,
+chave_acesso VARCHAR(10),
+voto INT,
+protocolo_votacao VARCHAR(12),
+data_votacao VARCHAR(100)
 );
 
 SELECT * FROM eleitores;
