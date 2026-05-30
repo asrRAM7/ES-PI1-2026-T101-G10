88e0a8e6a0d1f287dc594495f38a9be6d4a85f46
asrRAM7 <ramiro.asr@puccampinas.edu.br>
2026-05-30 19:14:15 -0300

    Refactor logging functions and add criar_log

--- auditoria.py
+++ auditoria.py
@@ -1,5 +1,14 @@
 from datetime import datetime
 
+def criar_log():
+    try:
+        with open("logs.txt", "r", encoding="utf-8"):
+            pass
+    except FileNotFoundError:
+        with open("logs.txt", "w", encoding="utf-8") as arquivo:
+            arquivo.write(f"Horário: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} --- Registro: Arquivo de Logs criado.\n")
+            pass
+
 def registrar_log(entrada):
     try:
         with open("logs.txt", "r", encoding="utf-8"):
@@ -8,10 +17,10 @@ def registrar_log(entrada):
         with open("logs.txt", "w", encoding="utf-8") as arquivo:
             arquivo.write(f"Horário: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} --- Registro: Arquivo de Logs criado.\n")
             pass
-    
+
     with open("logs.txt", "a", encoding="utf-8") as arquivo:
         arquivo.write(f"Horário: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} --- Registro: {entrada}\n")
-
+    
 def ler_log():
     try:
         with open("logs.txt", "r", encoding="utf-8") as arquivo:
